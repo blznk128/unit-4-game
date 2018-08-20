4d4358eb336ee8d66ef8c81301285685245ced53
Benny Borunda <blznk128@yahoo.com>
2018-08-20 10:49:53 -0600

    final version

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # unit-4-game
+
+I had a tough time seperating the random numbers the Math.random method returned. I originally just re wrote the
+lines of code for each crystal but realized there has to be a easier, shorter way to get the same result.
+Before I sat up a function, but realized all 4 crystal shared the same random number that was generated.
+Then the issue I had was each time I clicked the crystal, it kept picking a different number everytime, I fixed that, then
+it wouldnt add properly after it chose a new number. Eventually I got those issues fixed, then I realized when the random number
+players have to get reset to another number, it wouldn't update it on the screen. This homework definitely challenged everything
+I know about javascript and had to read the activities over and over and also read a book about javascript to figure it out.
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,138 +1,51 @@
-var random = "";
-random = Math.ceil((Math.random() * 100)) + 19;
-    console.log(random);
+generateNumbers();
+    var wins = 0;
+    $("#wins").text(wins);
+    var losses = 0;
+    $("#losses").text(losses);
 
-$("#hit").text(random);
+    function crystalClick(crystalValue) {
+        score = crystalValue + score;
+        $("#score").text("Your score: " + score);
+        if (score === target) {
+            alert("You win!");
+            wins++;
+            $("#wins").text(wins);
+            generateNumbers();      
+        }
+        if (score > target) {
+            alert("You lost =(");
+            losses++;
+            $("#losses").text(losses);
+            generateNumbers();
+        }
+    };
 
-var userScore = 0;
-var crystal1 = Math.floor(Math.random()*11+1);
-var crystal2 = Math.floor(Math.random()*11+1);
-var crystal3 = Math.floor(Math.random()*11+1);
-var crystal4 = Math.floor(Math.random()*11+1);
-var wins = 0;
-var losses = 0;
-function restart() {
-    random = Math.floor(Math.random()*101+19);
-      console.log(random)
-    $("#hit").text(random);
-    crystal1= Math.floor(Math.random()*11+1);
-    crystal2= Math.floor(Math.random()*11+1);
-    crystal3= Math.floor(Math.random()*11+1);
-    crystal4= Math.floor(Math.random()*11+1);
-    userScore= 0;
-    $("#score").text(userScore);
-    } 
-
-$(".crystal").on("click", function() {
-    userScore = userScore + crystal1;
-        console.log(userScore);
-    $("#score").text("Your Score: " + userScore)
-
-
-    if (userScore === random) {
-        alert("you win");
-        $("#wins").text(wins+=1);
-        random = Math.ceil((Math.random() * 100)) + 19;
-        $("#hit").text(random);
-        userScore = 0
-        $("#score").text("Your Score: " + userScore);
-        restart();
-        
-        
+    function generateNumbers() {
+        target = Math.floor((Math.random() * 100) +19);
+        $("#target").text(target);
+        crystal1 = Math.floor((Math.random() * 11) +1);
+        crystal2 = Math.floor((Math.random() * 11) +1);
+        crystal3 = Math.floor((Math.random() * 11) +1);
+        crystal4 = Math.floor((Math.random() * 11) +1);
+        score = 0;
+        $("#score").text("Your score: " + score);
     }
-    if (userScore > random) {
-        alert("lose");
-        $("#losses").text(losses+=1);
-        random = Math.ceil((Math.random() * 100)) + 19;
-        $("#hit").text(random);
-        userScore = 0;
-        $("#score").text("Your Score: " + userScore);
-        restart();
-    }
-});
 
-$(".crystal2").on("click", function() {
-    userScore = userScore + crystal2;
-        console.log(userScore);
-    $("#score").text("Your Score: " + userScore)
-
-
-    if (userScore === random) {
-        alert("you win");
-        $("#wins").text(wins+=1);
-        random = Math.ceil((Math.random() * 100)) + 19;
-        $("#hit").text(random);
-        userScore = 0
-        $("#score").text("Your Score: " + userScore);
-        restart();
+    $(".crystal").on("click", function() {
+        crystalClick(crystal1);
         
-        
-    }
-    if (userScore > random) {
-        alert("lose");
-        $("#losses").text(losses+=1);
-        random = Math.ceil((Math.random() * 100)) + 19;
-        $("#hit").text(random);
-        userScore = 0;
-        $("#score").text("Your Score: " + userScore);
-        restart();
-    }
-});
+    })
+    $(".crystal2").on("click", function () {
+        crystalClick(crystal2);
+    })
 
-$(".crystal3").on("click", function() {
-    userScore = userScore + crystal3;
-        console.log(userScore);
-    $("#score").text("Your Score: " + userScore)
+    $(".crystal3").on("click", function (){
+        crystalClick(crystal3)
+    })
 
-
-    if (userScore === random) {
-        alert("you win");
-        $("#wins").text(wins+=1);
-        random = Math.ceil((Math.random() * 100)) + 19;
-        $("#hit").text(random);
-        userScore = 0
-        $("#score").text("Your Score: " + userScore);
-        restart();
-        
-        
-    }
-    if (userScore > random) {
-        alert("lose");
-        $("#losses").text(losses+=1);
-        random = Math.ceil((Math.random() * 100)) + 19;
-        $("#hit").text(random);
-        userScore = 0;
-        $("#score").text("Your Score: " + userScore);
-        restart();
-    }
-});
-
-$(".crystal4").on("click", function() {
-    userScore = userScore + crystal4;
-        console.log(userScore);
-    $("#score").text("Your Score: " + userScore)
-
-
-    if (userScore === random) {
-        alert("you win");
-        $("#wins").text(wins+=1);
-        random = Math.ceil((Math.random() * 100)) + 19;
-        $("#hit").text(random);
-        userScore = 0
-        $("#score").text("Your Score: " + userScore);
-        restart();
-        
-        
-    }
-    if (userScore > random) {
-        alert("lose");
-        $("#losses").text(losses+=1);
-        random = Math.ceil((Math.random() * 100)) + 19;
-        $("#hit").text(random);
-        userScore = 0;
-        $("#score").text("Your Score: " + userScore);
-        restart();
-    }
-});
+    $(".crystal4").on("click", function (){
+        crystalClick(crystal4)
+    })
     
 
